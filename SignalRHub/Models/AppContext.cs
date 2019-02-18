690a05fcb7f8faff74bb00c06bf94df7e89acdb5
masudbs23 <masud@bs-23.net>
2019-02-18 12:04:55 +0600

    updated

---
+++ SignalRHub/Models/AppContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRHub
+{
+    public class AppContext : IdentityDbContext<IdentityUser>
+    {
+        public AppContext(DbContextOptions<AppContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Notification> Notifications { set; get; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Connection> Connections { get; set; }
+    }
+}
